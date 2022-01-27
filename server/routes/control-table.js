2c5777e58e8d99e99e7edaf8e51d7d76ad7eb8f6
Jeremy Luzon <lznjeremy@gmail.com>
2022-01-27 22:51:17 +0200

    Update control-table.js

--- server/routes/control-table.js
+++ server/routes/control-table.js
@@ -7,7 +7,7 @@ router.post('/address', async(req, res) => {
     try {
         const address_collection = database.collection('control_table'); 
         const address = address_collection.doc(req.body.address); 
-        await address.set(req.body);
+        await address.set(req.body, {merge: true});
         res.send({ error: 0, message: `Address successfully added : ${req.body.address}` });
     } catch (err) {
         console.log(err);
@@ -15,12 +15,24 @@ router.post('/address', async(req, res) => {
     }
 })
 
-// Get addresses from control-table
+// Get all  addresses
 router.get('/address', async(req, res) => {
     try {
         const snapshot = await database.collection('control_table').get()
         const rows = snapshot.docs.map(doc => doc.data());
         res.send(rows);
+    } catch (err) {
+        console.log(err);
+        res.send({ error: 1, message: err.message });
+    }
+})
+
+// Update address
+router.put('/address', async(req, res) => {
+    try {
+        console.log(req.body)
+        const result = await database.collection("control_table").doc(req.body.address).update(req.body);
+        res.send(result);
     } catch (err) {
         console.log(err);
         res.send({ error: 1, message: err.message });
